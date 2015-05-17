7c04abd0232827f5f86f1f74174db097192dc180
meskarune <meskarune@gmail.com>
2015-05-16 21:05:01 -0400

    trying to switch to xcffib

--- windows.py
+++ windows.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python2
 
-from Xlib import display, X
+#from Xlib import display, X
 
-display = display.Display()
-screen = display.screen()
-root = screen.root
+#display = display.Display()
+#screen = display.screen()
+#root = screen.root
+#tree = root.query_tree()
+#wins = tree.children
+
+#for win in wins: 
+#    print win.get_wm_name()
+
+import xcffib
+
+display = xcffib.connect()
+screen = display.get_setup()
+root = screen.roots[0].root
+
+#display = xcffib.connect()
+#screen = display.get_setup().roots[0]
+#root = screen.root
 tree = root.query_tree()
-wins = tree.children
+wins= tree.children
 
-for win in wins: 
+for win in wins:
     print win.get_wm_name()
+
+#xcb_query_tree_reply_t;

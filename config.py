57829eabe779ea477c0899d924f27d23fc38837e
meskarune <meskarune@gmail.com>
2015-05-18 12:48:59 -0400

    added configuration option to close all windows using wmctrl

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 # The command used to lock the screen
 lock_command = "lock"
 
+# The command used to close all open windows gracefully
+# default reqires that wmctrl be installed
+close_command = "wmctrl -l -p | awk '{print $3}' | while read PID; do kill -15
+$PID; done"
+
 # The command used to logout from the current session
 # i3
 # logout_command = "i3-msg exit"
@@ -17,5 +22,8 @@ logout_command = "herbstclient quit"
 suspend_command = "pm-suspend"
 hibernate_command = "pm-hibernate"
 
+# The command run to reboot the computer
 reboot_command = "systemctl reboot"
+
+# The command run to turn off the computer
 shutdown_command = "systemctl poweroff"
